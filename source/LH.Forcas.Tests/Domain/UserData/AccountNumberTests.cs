a6aca36b5cbfdcd5080cc21c1dadc06e25d5d15e
Lukas Holota <l.holota@outlook.com>
2017-01-31 16:23:49 +0100

    AccountNumber refactored to full IBAN logic

--- source/LH.Forcas.Tests/Domain/UserData/AccountNumberTests.cs
+++ source/LH.Forcas.Tests/Domain/UserData/AccountNumberTests.cs
@@ -8,118 +8,171 @@
     public class AccountNumberTests
     {
         [TestFixture]
-        public class ParseTests
+        public class CzTests
         {
             [Test]
-            public void ShouldThrowIfInputIsEmpty()
+            public void ShouldCreateFromIban()
             {
-                Assert.Catch<ArgumentException>(() => AccountNumber.Parse(null));
+                var number = AccountNumber.FromIban("CZ2155000000002254171001");
+                Assert.AreEqual("2254171001/5500", number.LocalFormat);
             }
 
             [Test]
-            public void ShouldThrowIfFormatDoesNotMatch()
+            public void ShouldThrowWithInvalidIban()
             {
-                Assert.Catch<ArgumentException>(() => AccountNumber.Parse("sdfsdfgsd456fg46sd"));
+                Assert.Throws<FormatException>(() => AccountNumber.FromIban("CZ9955000000002254171001"));
             }
 
             [Test]
-            public void ShouldParseFullNumber()
+            public void ShouldThrowWithInvalidLocal()
             {
-                var result = AccountNumber.Parse("123-567/5500");
-
-                Assert.AreEqual(123, result.Prefix);
-                Assert.AreEqual(567, result.Number);
-                Assert.AreEqual(5500, result.BankRoutingCode);
+                Assert.Throws<FormatException>(() => AccountNumber.FromCzLocal("AADFHOISDFH"));
             }
 
             [Test]
-            public void ShouldParseNumberWithoutPrefix()
+            public void ShouldThrowOnEmptyIban()
             {
-                var result = AccountNumber.Parse("567/5500");
-
-                Assert.AreEqual(567, result.Number);
-                Assert.AreEqual(5500, result.BankRoutingCode);
+                Assert.Throws<ArgumentNullException>(() => AccountNumber.FromIban(""));
             }
+
+            [Test]
+            public void ShouldThrowOnEmptyLocal()
+            {
+                Assert.Throws<ArgumentNullException>(() => AccountNumber.FromCzLocal(""));
+            }
+
+            [Test]
+            public void ShouldCreateFromLocal()
+            {
+                var number = AccountNumber.FromCzLocal("2254171001/5500");
+                Assert.AreEqual("CZ2155000000002254171001", number.Iban);
+            }
+
+            [Test]
+            public void ShouldReformatLocal()
+            {
+                var number = AccountNumber.FromCzLocal("0000-2254171001/5500");
+                Assert.AreEqual("2254171001/5500", number.LocalFormat);
+            }
+
+
+            //[Test]
+            //public void ShouldThrowIfInputIsEmpty()
+            //{
+            //    Assert.Catch<ArgumentException>(() => AccountNumber.Parse(null));
+            //}
+
+            //[Test]
+            //public void ShouldThrowIfFormatDoesNotMatch()
+            //{
+            //    Assert.Catch<ArgumentException>(() => AccountNumber.Parse("sdfsdfgsd456fg46sd"));
+            //}
+
+            //[Test]
+            //public void ShouldParseFullNumber()
+            //{
+            //    var result = AccountNumber.Parse("123-567/5500");
+
+            //    Assert.AreEqual(123, result.Prefix);
+            //    Assert.AreEqual(567, result.Number);
+            //    Assert.AreEqual(5500, result.BankRoutingCode);
+            //}
+
+            //[Test]
+            //public void ShouldParseNumberWithoutPrefix()
+            //{
+            //    var result = AccountNumber.Parse("567/5500");
+
+            //    Assert.AreEqual(567, result.Number);
+            //    Assert.AreEqual(5500, result.BankRoutingCode);
+            //}
         }
 
         [TestFixture]
         public class EqualityTests
         {
-            private AccountNumber numberA = new AccountNumber {Prefix = 123, Number = 567, BankRoutingCode = 5500};
-            private AccountNumber numberAClone = new AccountNumber {Prefix = 123, Number = 567, BankRoutingCode = 5500};
-            private AccountNumber numberB = new AccountNumber {Prefix = 123, Number = 567, BankRoutingCode = 5501};
+            //private AccountNumber numberA = new AccountNumber { Prefix = 123, Number = 567, BankRoutingCode = 5500};
+            //private AccountNumber numberAClone = new AccountNumber { Prefix = 123, Number = 567, BankRoutingCode = 5500};
+            //private AccountNumber numberB = new AccountNumber { Prefix = 123, Number = 567, BankRoutingCode = 5501};
 
-            [Test]
-            public void EqualsShouldReturnTrueForSameValues()
-            {
-                Assert.IsTrue(this.numberA.Equals(this.numberAClone));
-            }
+            //[Test]
+            //public void EqualsShouldReturnTrueForSameValues()
+            //{
+            //    Assert.IsTrue(this.numberA.Equals(this.numberAClone));
+            //}
 
-            [Test]
-            public void EqualsShouldReturnFalseForDifferentValues()
-            {
-                Assert.IsFalse(this.numberA.Equals(this.numberB));
-            }
+            //[Test]
+            //public void EqualsShouldReturnFalseForDifferentValues()
+            //{
+            //    Assert.IsFalse(this.numberA.Equals(this.numberB));
+            //}
 
-            [Test]
-            public void GetHashCodeShouldReturnSameCodesForSameValues()
-            {
-                Assert.AreEqual(this.numberA.GetHashCode(), this.numberAClone.GetHashCode());
-            }
+            //[Test]
+            //public void GetHashCodeShouldReturnSameCodesForSameValues()
+            //{
+            //    Assert.AreEqual(this.numberA.GetHashCode(), this.numberAClone.GetHashCode());
+            //}
 
-            [Test]
-            public void GetHashCodeShouldReturnDifferentCodesForDifferentValues()
-            {
-                Assert.AreNotEqual(this.numberA.GetHashCode(), this.numberB.GetHashCode());
-            }
+            //[Test]
+            //public void GetHashCodeShouldReturnDifferentCodesForDifferentValues()
+            //{
+            //    Assert.AreNotEqual(this.numberA.GetHashCode(), this.numberB.GetHashCode());
+            //}
 
-            [Test]
-            public void EqualOperatorShouldReturnTrueForSameValues()
-            {
-                Assert.IsTrue(this.numberA == this.numberAClone);
-            }
+            //[Test]
+            //public void EqualOperatorShouldReturnTrueForSameValues()
+            //{
+            //    Assert.IsTrue(this.numberA == this.numberAClone);
+            //}
 
-            [Test]
-            public void EqualOperatorShouldReturnFalseForDifferentValues()
-            {
-                Assert.IsFalse(this.numberA == this.numberB);
-            }
+            //[Test]
+            //public void EqualOperatorShouldReturnFalseForDifferentValues()
+            //{
+            //    Assert.IsFalse(this.numberA == this.numberB);
+            //}
 
-            [Test]
-            public void InequalOperatorShouldReturnTrueForDifferentValues()
-            {
-                Assert.IsTrue(this.numberA != this.numberB);
-            }
+            //[Test]
+            //public void InequalOperatorShouldReturnTrueForDifferentValues()
+            //{
+            //    Assert.IsTrue(this.numberA != this.numberB);
+            //}
 
-            [Test]
-            public void InequalOperatorShouldReturnFalseForSameValues()
-            {
-                Assert.IsFalse(this.numberA != this.numberAClone);
-            }
+            //[Test]
+            //public void InequalOperatorShouldReturnFalseForSameValues()
+            //{
+            //    Assert.IsFalse(this.numberA != this.numberAClone);
+            //}
         }
 
         [TestFixture]
-        public class ToStringTests
+        public class IbanCalculationTests
         {
             [Test]
-            public void NumberWithoutPrefixShouldRenderWithoutDash()
+            public void ValidateIbanSuccess()
             {
-                var number = new AccountNumber();
-                number.BankRoutingCode = 5500;
-                number.Number = 123;
-
-                Assert.AreEqual("123/5500", number.ToString());
+                const string iban = "CZ2155000000002254171001";
+                Assert.IsTrue(AccountNumber.IsIbanValid(iban));
             }
 
             [Test]
-            public void NumberWithPrefixShouldRenderInFull()
+            public void ValidateIbanInvalid()
             {
-                var number = new AccountNumber();
-                number.BankRoutingCode = 5500;
-                number.Number = 123;
-                number.Prefix = 567;
+                const string iban = "CZ2155010000002254171001";
+                Assert.IsFalse(AccountNumber.IsIbanValid(iban));
+            }
 
-                Assert.AreEqual("567-123/5500", number.ToString());
+            [Test]
+            public void ValidateIbanInvalidFormat()
+            {
+                const string iban = "CZ21 5501 0000 0022 5417 1001";
+                Assert.Throws<FormatException>(() => AccountNumber.IsIbanValid(iban));
+            }
+
+            [Test]
+            public void CalculateChecksumSuccess()
+            {
+                var number = AccountNumber.FromCzLocal("2254171001/5500");
+                Assert.AreEqual("CZ2155000000002254171001", number.Iban);
             }
         }
     }
--- source/LH.Forcas/LH.Forcas/Domain/UserData/AccountNumber.cs
+++ source/LH.Forcas/LH.Forcas/Domain/UserData/AccountNumber.cs
@@ -1,81 +1,150 @@
 ﻿namespace LH.Forcas.Domain.UserData
 {
     using System;
+    using System.Collections.Generic;
+    using System.Text;
     using System.Text.RegularExpressions;
-
-    /*
-     * Impl in base -> ToString override (To GetIban())
-     * Abstract -> GetIban, ToLocalString()
-     * Specific only -> Parse method
-     */
 
     public struct AccountNumber
     {
-        private static readonly Regex ParseRegex = new Regex("(?:(?<prefix>\\d+)-)?(?<number>\\d+)/(?<code>\\d{4})", RegexOptions.CultureInvariant);
+        private static readonly IDictionary<string, Func<string, AccountNumber>> IBANParsers = new Dictionary<string, Func<string, AccountNumber>>
+        {
+            { "CZ", FromCzIban }
+        };
 
-        public static AccountNumber Parse(string value)
+        public static AccountNumber FromIban(string iban)
+        {
+            if (string.IsNullOrEmpty(iban))
+            {
+                throw new ArgumentNullException(nameof(iban));
+            }
+
+            if (iban.Length < 4)
+            {
+                throw new FormatException("The IBAN has to be at least 4 characters long.");
+            }
+
+            if (!IsIbanValid(iban))
+            {
+                throw new FormatException($"The iban {iban} is invalid (checksum validation failed).");
+            }
+
+            Func<string, AccountNumber> parserFunc;
+            var prefix = iban.Substring(0, 2).ToUpperInvariant();
+
+            if (!IBANParsers.TryGetValue(prefix, out parserFunc))
+            {
+                throw new NotSupportedException($"The IBAN prefix {prefix} is not supported.");
+            }
+
+            return parserFunc.Invoke(iban);
+        }
+
+        public static bool IsIbanValid(string iban)
+        {
+            return CalculateIbanRemainder(iban) == 1;
+        }
+
+        #region Czech Republic
+
+        private static readonly Regex CzLocalRegex = new Regex("(?:(?<prefix>\\d+)-)?(?<number>\\d+)/(?<bban>\\d{4})", RegexOptions.CultureInvariant);
+        private static readonly Regex CzIbanRegex = new Regex("CZ(\\d{2})(?<bban>\\d{4})(?<prefix>\\d{6})(?<number>\\d{10})", RegexOptions.CultureInvariant);
+
+        public static AccountNumber FromCzLocal(string value)
         {
             if (string.IsNullOrEmpty(value))
             {
-                throw new ArgumentException("The input value cannot be empty.", nameof(value));
+                throw new ArgumentNullException(nameof(value), "The input value cannot be empty.");
             }
 
-            var match = ParseRegex.Match(value);
+            var match = CzLocalRegex.Match(value);
 
             if (!match.Success)
             {
-                throw new ArgumentException("The input value does not match the expected format.", nameof(value));
+                throw new FormatException("The input value does not match the expected format.");
             }
-
-            var result = new AccountNumber();
-            result.BankRoutingCode = int.Parse(match.Groups["code"].Value);
-            result.Number = long.Parse(match.Groups["number"].Value);
 
             var prefix = match.Groups["prefix"].Value;
-            if (!string.IsNullOrEmpty(prefix))
-            {
-                result.Prefix = int.Parse(prefix);
-            }
+            var number = match.Groups["number"].Value;
+            var bban = match.Groups["bban"].Value;
 
-            return result;
+            var iban = ToCzIban(prefix, number, bban);
+            var localFormat = ToCzLocal(prefix, number, bban);
+
+            return new AccountNumber(iban, localFormat);
         }
 
-        public int Prefix { get; set; }
+        private static AccountNumber FromCzIban(string iban)
+        {
+            if (string.IsNullOrEmpty(iban))
+            {
+                throw new ArgumentNullException(nameof(iban), "The input value cannot be empty.");
+            }
 
-        public long Number { get; set; }
+            var match = CzIbanRegex.Match(iban);
 
-        public int BankRoutingCode { get; set; }
+            if (!match.Success)
+            {
+                throw new ArgumentException("The input value does not match the expected format.", nameof(iban));
+            }
+
+            var prefix = match.Groups["prefix"].Value;
+            var number = match.Groups["number"].Value;
+            var bban = match.Groups["bban"].Value;
+
+            var localFormat = ToCzLocal(prefix, number, bban);
+
+            return new AccountNumber(iban, localFormat);
+        }
+
+        private static string ToCzLocal(string prefix, string number, string bban)
+        {
+            if (!string.IsNullOrEmpty(prefix) && !string.IsNullOrEmpty(prefix.TrimStart('0')))
+            {
+                return $"{prefix}-{number}/{bban}";
+            }
+
+            return $"{number}/{bban}";
+        }
+
+        private static string ToCzIban(string prefix, string number, string bban)
+        {
+            var iban = $"CZ00{bban}{prefix.PadLeft(6, '0')}{number.PadLeft(10, '0')}";
+
+            return CalculateIbanChecksum(iban);
+        }
+
+        #endregion
+
+        private AccountNumber(string iban, string localFormat)
+        {
+            this.Iban = iban;
+            this.LocalFormat = localFormat;
+        }
+
+        public string Iban { get; }
+
+        public string LocalFormat { get; }
 
         public override string ToString()
         {
-            if (this.Prefix != default(int))
-            {
-                return $"{this.Prefix}-{this.Number}/{this.BankRoutingCode}";
-            }
-
-            return $"{this.Number}/{this.BankRoutingCode}";
+            return $"IBAN: {this.Iban}, Local: {this.LocalFormat}";
         }
 
         public bool Equals(AccountNumber other)
         {
-            return this.BankRoutingCode == other.BankRoutingCode && this.Number == other.Number && this.Prefix == other.Prefix;
+            return this.Iban == other.Iban;
         }
 
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
-            return obj is AccountNumber && this.Equals((AccountNumber) obj);
+            return obj is AccountNumber && this.Equals((AccountNumber)obj);
         }
 
         public override int GetHashCode()
         {
-            unchecked
-            {
-                long hashCode = this.BankRoutingCode;
-                hashCode = (hashCode*397) ^ this.Number;
-                hashCode = (hashCode*397) ^ this.Prefix;
-                return (int)hashCode;
-            }
+            return this.Iban.GetHashCode();
         }
 
         public static bool operator ==(AccountNumber left, AccountNumber right)
@@ -86,6 +155,61 @@
         public static bool operator !=(AccountNumber left, AccountNumber right)
         {
             return !left.Equals(right);
+        }
+
+        private static string CalculateIbanChecksum(string zeroIban)
+        {
+            var checkDigits = 98 - CalculateIbanRemainder(zeroIban);
+
+            var builder = new StringBuilder(zeroIban);
+            builder[2] = (char)(checkDigits/10+'0');
+            builder[3] = (char)(checkDigits%10+'0');
+
+            return builder.ToString();
+        }
+
+        private static int CalculateIbanRemainder(string iban)
+        {
+            // Code in this method was inspired by code created DvdKhl (https://gist.github.com/DvdKhl/6139665), released under MIT License
+
+            if (iban.Length < 4)
+            {
+                throw new FormatException();
+            }
+
+            var checksum = 0;
+
+            for (var i = 0; i < iban.Length; i++)
+            {
+                int value;
+
+                var c = iban[(i + 4) % iban.Length];
+
+                if (char.IsDigit(c))
+                {
+                    value = c - '0';
+                }
+                else if (char.IsLetter(c) && char.IsUpper(c))
+                {
+                    value = c - 'A';
+                    checksum = (checksum * 10 + (value / 10 + 1)) % 97;
+                    value %= 10;
+                }
+                else if (char.IsLetter(c) && char.IsLower(c))
+                {
+                    value = c - 'a';
+                    checksum = (checksum * 10 + (value / 10 + 1)) % 97;
+                    value %= 10;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+
+                checksum = (checksum * 10 + value) % 97;
+            }
+
+            return checksum;
         }
     }
 }
