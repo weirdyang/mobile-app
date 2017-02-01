8a14df4c403dd9570d73262d3c6d72b597b5a098
Lukas Holota <l.holota@outlook.com>
2017-02-01 09:26:18 +0100

    Number to color converter added

---
+++ source/LH.Forcas.Tests/Views/Reusables/Converters/NumberColorIndicatorConverter.cs
@@ -0,0 +1,44 @@
+﻿namespace LH.Forcas.Tests.Views.Reusables.Converters
+{
+    using System;
+    using System.Globalization;
+    using Forcas.Views.Reusable.Converters;
+    using NUnit.Framework;
+    using Xamarin.Forms;
+
+    [TestFixture]
+    public class NumberColorIndicatorConverterTests
+    {
+        private NumberColorIndicatorConverter converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.converter = new NumberColorIndicatorConverter();
+            this.converter.PositiveColor = Color.Green;
+            this.converter.NegativeColor = Color.Red;
+        }
+
+        [Test]
+        public void ShouldConvertPositiveNumberToPositiveColor()
+        {
+            var color = this.converter.Convert(10m, typeof(Color), null, CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(Color.Green, color);
+        }
+
+        [Test]
+        public void ShouldConvertNegativeNumberToNegativeColor()
+        {
+            var color = this.converter.Convert(-10m, typeof(Color), null, CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(Color.Red, color);
+        }
+
+        [Test]
+        public void ShouldThrowOnInvalidValue()
+        {
+            Assert.Throws<ArgumentException>(() => this.converter.Convert("Hello", typeof(Color), null, CultureInfo.CurrentCulture));
+        }
+    }
+}
---
+++ source/LH.Forcas/LH.Forcas/Views/Reusable/Converters/NumberColorIndicatorConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace LH.Forcas.Views.Reusable.Converters
+{
+    using System;
+    using System.Globalization;
+    using Xamarin.Forms;
+    public class NumberColorIndicatorConverter : IValueConverter
+    {
+        public Color PositiveColor { get; set; }
+
+        public Color NegativeColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is decimal))
+            {
+                throw new ArgumentException("Only decimal value conversion is supported.", nameof(value));
+            }
+
+            var number = (decimal) value;
+
+            return number >= 0 ? this.PositiveColor : this.NegativeColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Should not be used");
+        }
+    }
+}
